e38d002744e8d2c29adca418f290b88893cab79d
nickclone2k6-boop <nickclone2k6@gmail.com>
2025-11-03 22:40:14 +0700

    Add files via upload
    làm xong khung sườn cho Ve <== VeThuong <== VeVIP + tạo thêm file interface Ve

---
+++ IVe.java
@@ -0,0 +1,10 @@
+package src;
+
+public interface IVe {
+    void hienThiThongTin();
+    double tinhTongTien();
+    String getMaVe();
+    String getMaChuyenBay();
+    String getMaGhe();
+    String getLoaiVe();
+}
